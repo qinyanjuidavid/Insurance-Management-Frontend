93496da9e6b45289f6dbaec2427e34b2dc5d086d
qinyanjuidavid <davidkinyanjui052@gmail.com>
2023-02-13 18:42:14 +0300

    :bookmark: implemented the beneficiaries services

--- src/app/service/beneficiary.service.ts
+++ src/app/service/beneficiary.service.ts
@@ -16,4 +16,29 @@ export class BeneficiaryService {
       .get<Beneficiary[]>(this.apiUrl + 'beneficiaries')
       .pipe(tap((beneficiary) => console.log('Beneficiary:::', beneficiary)));
   }
+
+  getBeneficiary(id: number): Observable<Beneficiary> {
+    return this.http.get<Beneficiary>(this.apiUrl + 'beneficiaries/' + id);
+  }
+
+  addBeneficiary(beneficiary: Beneficiary): Observable<Beneficiary> {
+    http: return this.http.post<Beneficiary>(
+      this.apiUrl + 'beneficiaries/',
+      beneficiary
+    );
+  }
+
+  updateBeneficiary(
+    id: number,
+    beneficiary: Beneficiary
+  ): Observable<Beneficiary> {
+    return this.http.put<Beneficiary>(
+      this.apiUrl + 'beneficiaries/' + id + '/',
+      beneficiary
+    );
+  }
+
+  deleteBeneficiary(id: number): Observable<Beneficiary> {
+    return this.http.delete<Beneficiary>(this.apiUrl + 'beneficiaries/' + id);
+  }
 }
